6a022516405cc0fc13a32cb08b729105530da2aa
ALVARO-DIEGO-VALENCIA-MARTINEZ <alvarodi.96@gmail.com>
2025-03-26 08:08:24 -0500

    Delete teller yaris directory

--- teller yaris/ejercicio20.js
+++
@@ -1,5 +0,0 @@
-function convertir() {
-    let km = document.getElementById("kilometros").value;
-    let millas = km * 0.621371; 
-    document.getElementById("millas").textContent = millas.toFixed(2); 
-}
